0a872a83ea2d659d60e85dc615d040224fe40d3a
Kwon-DoHee <152317074+seamooll@users.noreply.github.com>
2025-02-12 22:52:49 +0900

    [Silver IV] Title: 주몽, Time: 76 ms, Memory: 2080 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1940. 주몽/주몽.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, a[15004], cnt;
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	cin >> n >> m;
+	for(int i = 0; i < n; i++) cin >> a[i];
+	if(m > 200000) cout << 0 << "\n";
+	else{
+		for(int i = 0; i < n; i++){
+			for(int j = i + 1; j < n; j++){
+				if(a[i] + a[j] == m) cnt++;
+			}
+		}
+		cout << cnt << "\n";
+	}
+}
